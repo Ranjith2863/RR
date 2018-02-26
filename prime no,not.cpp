636c74207ed47e84d4318decf6a58739be4c870b
Ranjith2863 <36299500+Ranjith2863@users.noreply.github.com>
2018-02-26 17:30:33 +0530

    Update prime no,not.cpp

--- prime no,not.cpp
+++ prime no,not.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	int num,divisor,i,count=0;
 	cout<<"Enter a number\n";
 	cin>>num;
 	for(i=2;i<=num/2;i++)
 	{
-		if(num%i==0)
-		{
-			count+=1;
-		}
+	if(num%i==0)
+	{
+	count+=1;
 	}
+}
 	if(count==0)
 	{
-		cout<<"YES";
+	cout<<"YES";
 	}
 	else
 	{
-		cout<<"NO";
+	cout<<"NO";
 	}
 	return 0;
 }
