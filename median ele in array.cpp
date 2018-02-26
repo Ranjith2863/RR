73a8efb0350ea4d4c550bb2961293325027ca198
Ranjith2863 <36299500+Ranjith2863@users.noreply.github.com>
2018-02-26 17:59:31 +0530

    Create median ele in array.cpp

---
+++ median ele in array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n,i,j,count;
+	cout<<"Enter the value of N\n";
+	cin>>n;
+	int *arr=new int[n];
+	cout<<"Enter the value of array\n";
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	for(i=0;i<n-1;i++)
+	{
+		count=0;
+		for(j=0;j<n-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				swap(arr[j],arr[j+1]);
+				count++;
+			}
+		}
+		if(count==0)
+		break;
+	}
+	cout<<"Middle element\n";
+	cout<<arr[n/2];
+	return 0;
+}
