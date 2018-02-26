d887a0bb57b50461fa28d86fdbdab4740113db67
Ranjith2863 <36299500+Ranjith2863@users.noreply.github.com>
2018-02-26 17:36:44 +0530

    Update even between intervals.cpp

--- even between intervals.cpp
+++ even between intervals.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	int i,num1,num2;
 	cout<<"Enter two numbers\n";
@@ -8,9 +7,9 @@ int main() {
 	for(i=num1+1;i<num2;i++)
 	{
 		if(i%2==0)
-		cout<<i<<" ";
-		else
-		continue;
+cout<<i<<" ";
+else
+continue;
 	}
 	return 0;
 }
