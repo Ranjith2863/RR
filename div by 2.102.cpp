d5bbe4e4b377ace35fde41c85834ee1c594a9563
Ranjith2863 <36299500+Ranjith2863@users.noreply.github.com>
2018-02-28 17:41:45 +0530

    Create div by 2.102.cpp

---
+++ div by 2.102.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{int a;
+  cout<<"Enter your number";
+  cin>>a;
+  if(a%2==0)
+  {
+  a=a/2;
+  cout<<a;
+  }
+  else
+  {
+  cout<<a;
+  }
+  return 0;
+}
