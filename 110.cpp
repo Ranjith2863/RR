784c32132907022299eac4d55b9c43844a973c58
Ranjith2863 <36299500+Ranjith2863@users.noreply.github.com>
2018-05-21 12:07:48 +0530

    Create 110.cpp

---
+++ 110.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n,i,sum=0;
+
+
+cout<<"enter the number";
+
+cin>>n;
+for(i=0;i<=n;++i)
+{
+	sum=sum+i;
+	
+}
+cout<<sum;
+
+return 0;
+}
