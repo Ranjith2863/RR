e8452b5d043584efaa05317988a663a703baced1
Ranjith2863 <36299500+Ranjith2863@users.noreply.github.com>
2018-05-21 11:26:11 +0530

    Create 98.cpp

---
+++ 98.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ using namespace std;
+int main() 
+{
+	
+  int n,arr[30],i;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		cin>>arr[i];
+	}
+	
+  for(i=1;i<=n;i++)
+	{
+		
+    if(i!=arr[i])
+		{
+			cout<<i;
+			break;
+		}
+	}
+ 
+	
+  return 0;
+}
